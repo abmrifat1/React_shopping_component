f940ffa72302885a2c607b0711f875aacc932010
abmrifat1 <rifatabm1@gmail.com>
2019-10-13 14:54:56 +0600

    last

---
+++ src/components/ShoppingItem.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { StyleSheet,View ,Image,Text} from 'react-native';
+
+const Shopping = props => {
+    const {shopping}=props;
+    return(
+   
+             <View style={styles.listContent}>
+              <View style={styles.img}>
+                <Image
+                  source={shopping.img}
+                  style={styles.itemImage}
+                />
+              </View>
+              <View style={styles.itemDetails}>
+                <View >
+                  <Text style={style=styles.itemName}>{shopping.name}</Text>
+                </View>
+                <View style={styles.itemInfo}>
+                  <View>
+                    <Text style={styles.commonFontSize}>UOM</Text>
+                    <Text>{shopping.uom}</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.commonFontSize}>PACK SIZE</Text>
+                    <Text>{shopping.packSize}</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.commonFontSize}>QUANTITY</Text>
+                    <Text>{shopping.quantity}</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.commonFontSize}>PER UNIT</Text>
+                    <Text>{shopping.perUnit}</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.commonFontSize}>TOTAT</Text>
+                    <Text>{shopping.totalPrice}</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={styles.itemType}>
+                <Text style={styles.circle2}>
+                   
+                </Text>
+              </View>
+            </View>
+               
+             
+)};
+
+const styles = StyleSheet.create({
+    commonFontSize:{
+        fontSize: 10,
+    },
+    listContent: {
+        flexDirection: 'row',
+      },
+      img: {
+        flex: 2,
+      },
+      itemImage:{
+        width: 70, height: 90,
+      },
+      itemDetails: {
+        flex: 10,
+      },
+      itemInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+      },
+      itemType: {
+        flex: 1,
+        paddingTop: 10,
+      },
+      circle2: {
+        height: 10,
+        width: 10,
+        backgroundColor: '#145727',
+        borderRadius: 50,
+        marginLeft: 7,
+      },
+      itemName:{
+        fontSize: 18,
+         padding: 10,
+      },
+
+});
+
+export default Shopping;
